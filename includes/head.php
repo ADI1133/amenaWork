83295baaad83842aa96081516e2daff5dde65749
Idrees <71092332+ADI1133@users.noreply.github.com>
2023-01-18 09:17:10 +0400

    Add files via upload

---
+++ includes/head.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<link rel="shortcut icon" href="assets/img/favicon.ico" type="img/png" />
+	<!--====== Animate Css ======-->
+	<link rel="stylesheet" href="assets/css/animate.min.css">
+	<!--====== Bootstrap css ======-->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<!--====== Fontawesome css ======-->
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+	<!--====== Flaticon css ======-->
+	<link rel="stylesheet" href="assets/fonts/flaticon/flaticon.css" />
+	<!--====== Magnific Popup css ======-->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css" />
+	<!--====== Owl Carousel css ======-->
+	<link rel="stylesheet" href="assets/css/slick.css" />
+	<!--====== Nice Select ======-->
+	<link rel="stylesheet" href="assets/css/nice-select.css" />
+	<!--====== Bootstrap Datepicker ======-->
+	<link rel="stylesheet" href="assets/css/bootstrap-datepicker.css" />
+	<!--====== Default css ======-->
+	<link rel="stylesheet" href="assets/css/default.css" />
+	<!--====== Style css ======-->
+	<link rel="stylesheet" href="assets/css/style.css" />
